70591e810f4c68c5a52598a2e8bbb3915842d243
Babur Duisenov <bduisenov@gmail.com>
2020-02-26 20:01:33 +0100

    Polish router

--- src/main/scala/org/scalalang/boot/reactive/controller/UserController.scala
+++ src/main/scala/org/scalalang/boot/reactive/controller/UserController.scala
@@ -1,6 +1,8 @@
 package org.scalalang.boot.reactive.controller
 
-import org.scalalang.boot.reactive.core.RouterBuilder
+import java.net.URI
+
+import org.scalalang.boot.reactive.core.Router
 import org.scalalang.boot.reactive.core.document.Document
 import org.scalalang.boot.reactive.core.document.Document._
 import org.scalalang.boot.reactive.repository.UserEntity
@@ -16,11 +18,11 @@ trait UserController {
 class UserControllerImpl(private val getUserUseCase: Document => Either[String, Document],
                          private val saveUserUseCase: Document => Either[String, Document]) extends UserController {
 
-  val getUserRoute: Document => Either[String, Document] = new RouterBuilder[Document, String]()
-    .apply(route => route.flatMap(getUserUseCase))
+  val getUserRoute: Document => Either[String, Document] = Router[Document, String](route => route
+    .flatMap(getUserUseCase))
 
-  val createUserRoute: Document => Either[String, Document] = new RouterBuilder[Document, String]()
-    .apply(route => route.flatMap(saveUserUseCase))
+  val createUserRoute: Document => Either[String, Document] = Router[Document, String](route => route
+    .flatMap(saveUserUseCase))
 
   override def getUser(id: Long): Mono[ServerResponse] =
     getUserRoute(Document(userId -> id)) match {
@@ -32,7 +34,9 @@ class UserControllerImpl(private val getUserUseCase: Document => Either[String, 
 
   override def saveUser(json: Map[String, String]): Mono[ServerResponse] =
     createUserRoute(Document(user -> (() => UserEntity(name = json("name"))))) match {
-      case Right(doc) => ServerResponse.ok().bodyValue(doc.user)
+      case Right(doc) => doc.user.fold(ServerResponse.badRequest().bodyValue("not found")) {
+        user => ServerResponse.created(URI.create(user.id.get.toString)).bodyValue(user)
+      }
       case Left(error) => ServerResponse.badRequest().bodyValue(error)
     }
 }
--- src/main/scala/org/scalalang/boot/reactive/core/Router.scala
+++ src/main/scala/org/scalalang/boot/reactive/core/Router.scala
@@ -8,14 +8,11 @@ sealed class RouteHistoryRecord[T, P](private val in: T, private val out: Either
 
 sealed class RouteContext[T, P](val state: T, val historyRecords: List[RouteHistoryRecord[T, P]] = List())
 
-sealed case class ExecutionContext[T, P](historyRecords: List[RouteHistoryRecord[T, P]], result: Either[P, T])
-
 sealed trait RouterFunction[T, P] {
-  def apply(state: T, either: Either[P, T]): ExecutionContext[T, P] = {
-    if (either.isRight) internalApply(state, either) else new ExecutionContext[T, P](List(), either)
-  }
+  def apply(state: T, either: Either[P, T]): (List[RouteHistoryRecord[T, P]], Either[P, T]) =
+    if (either.isRight) internalApply(state, either) else (List(), either)
 
-  def internalApply(state: T, either: Either[P, T]): ExecutionContext[T, P] = ???
+  def internalApply(state: T, either: Either[P, T]): (List[RouteHistoryRecord[T, P]], Either[P, T]) = ???
 
   def execute(function: Either[P, T] => Either[P, T], either: Either[P, T]): (Either[P, T], Duration) = { // ???
     val startTime = System.nanoTime()
@@ -31,49 +28,44 @@ sealed trait RouterFunction[T, P] {
   }
 }
 
-sealed class RouterBuilder[T, P]  {
-  self =>
+sealed class RouterBuilder[T, P](val route: State[RouteContext[T, P], Either[P, T]]) {
 
-  def apply(route1: RouterBuilder[T, P] => RouterBuilder[T, P]): Router[T, P] = {
-    val builder = new RouterBuilder[T, P]()
-    route1(builder).build()
-  }
-  
-  var route: State[RouteContext[T, P], Either[P, T]] = State(context => (context, Right(context.state)))
+  def this() =
+    this(State(context => (context, Right(context.state))))
 
-  def flatMap(fun: T => Either[P, T]): self.type = {
-    route = route.flatMap(thunk(State.pure(simple(either => either.flatMap(fun)))))
-    this
-  }
+  def flatMap(fun: T => Either[P, T]): RouterBuilder[T, P] =
+    new RouterBuilder[T, P](route.flatMap(thunk(State.pure(simple(either => either.flatMap(fun))))))
 
-  def simple(function: Either[P, T] => Either[P, T]): RouterFunction[T, P] = {
+  def simple(function: Either[P, T] => Either[P, T]): RouterFunction[T, P] =
     new RouterFunction[T, P] {
-      override def internalApply(state: T, either: Either[P, T]): ExecutionContext[T, P] = {
+      override def internalApply(state: T, either: Either[P, T]): (List[RouteHistoryRecord[T, P]], Either[P, T]) = {
         val executionResult = execute(function, either)
         val rhr = createRouteHistoryRecord(either.right.get, executionResult)
 
-        new ExecutionContext[T, P](List(rhr), executionResult._1)
+        (List(rhr), executionResult._1)
       }
     }
-  }
 
   def thunk(stateM: State[RouteContext[T, P], RouterFunction[T, P]]): Either[P, T] => State[RouteContext[T, P], Either[P, T]] = either =>
     stateM.flatMap(fun => State(context => {
-      val execContext = fun(context.state, either)
+      val (historyRecords, result) = fun(context.state, either)
 
-      val historyRecords = execContext.historyRecords
-      val result = execContext.result
-
-      val updatedHistoryRecords = context.historyRecords ++ historyRecords
-      val updatedContext = new RouteContext[T, P](result.getOrElse(context.state), updatedHistoryRecords)
+      val updatedContext = new RouteContext[T, P](result.getOrElse(context.state), context.historyRecords ++ historyRecords)
 
       (updatedContext, result)
     }))
 
-  def build(): Router[T, P] = {
+  def build(): Router[T, P] =
     new Router[T, P](initialState => route.run(new RouteContext[T, P](initialState)).value)
-  }
 }
+
+abstract private[core] class RouterFunctions {
+
+  def apply[T, P](route: RouterBuilder[T, P] => RouterBuilder[T, P]): Router[T, P] =
+    route(new RouterBuilder[T, P]()).build()
+}
+
+object Router extends RouterFunctions
 
 sealed class Router[T, P](val route: T => (RouteContext[T, P], Either[P, T])) extends (T => Either[P, T]) {
 
